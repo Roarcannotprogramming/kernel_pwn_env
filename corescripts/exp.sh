a263be071ac3520f351bd7c398b454c121677740
V1me <z1991998920@gmail.com>
2023-06-28 16:57:04 +0800

    set e

--- corescripts/exp.sh
+++ corescripts/exp.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 # run it as root
 
+set -e
+
 BASEDIR=$(dirname "$0")
 path=$BASEDIR/../pwn
 exp_path=$BASEDIR/../exp
-$BASEDIR/uncpio.sh && \
-gcc $exp_path/exp.c $exp_path/banzi.c -static -lkeyutils -lpthread -luring -g -o $exp_path/exp && \
-cp $exp_path/exp $path/core && \
-cp $path/init $path/core && \
-# cp $path/corescript/tools/* $path/core && \
-$BASEDIR/cpio.sh zip && \
-cd $path && \
-$path/run.sh -s
+$BASEDIR/uncpio.sh 
+gcc $exp_path/exp.c $exp_path/banzi.c -static -lkeyutils -lpthread -luring -g -o $exp_path/exp 
+cp $exp_path/exp $path/core 
+cp $path/init $path/core 
+# cp $path/corescript/tools/* $path/core 
+$BASEDIR/cpio.sh zip 
+cd $path && $path/run.sh -s

501614231263cbd4b2dc121b690e8ab03820e109
V1me <34340275+Roarcannotprogramming@users.noreply.github.com>
2024-05-11 13:43:51 +0800

    Update gdb_script.py

--- corescripts/gdb_script.py
+++ corescripts/gdb_script.py
@@ -2,6 +2,18 @@
 
 import gdb
 
+vmemmap_base = 0xffffea0000000000
+virtual_base = 0xffff888000000000
+
+def to_virt(page):
+    return (page - vmemmap_base)*0x40 + virtual_base
+
+def to_page(virt):
+    return (virt - virtual_base)//0x40 + vmemmap_base
+
+def to_uint64(x):
+    return x % (2**64)
+
 def container_of(member_ptr, parent_type, member_name):
     # 获取成员在父结构体中的偏移量
     parent_type = gdb.lookup_type(parent_type)
@@ -86,22 +98,86 @@ def find_slab_caches(name):
 
     return None
 
+class SlabFreeTracePoint(gdb.Breakpoint):
+    def stop(self):
+        pid = int(gdb.parse_and_eval("$lx_current()->pid"))
+        cpu = int(gdb.parse_and_eval("$lx_current()->thread_info.cpu"))
+        unix_cache = to_uint64(int(gdb.parse_and_eval("$cache_addr")))
+        free_cache = to_uint64(int(gdb.parse_and_eval("$rdi")))
+        slab = int(gdb.parse_and_eval("$rsi"))
+        addr = int(gdb.parse_and_eval("$rdx"))
+        # print("unix_cache: 0x{:x}\tfree_cache: 0x{:x}".format(unix_cache, free_cache))
+        if pid < 600:
+            return False
+        if cpu != 0:
+            return False
+        if unix_cache != free_cache:
+            return False
+        if self.cnt > 0:
+            self.cnt -= 1
+            gdb.execute("bt")
+        print("slab_free_unix\tpid: {}\tcpu: {}\tslab: 0x{:x}\taddr: 0x{:x}".format(pid, cpu, slab, addr))
+        return False
 
-slab_unix = []
-# name = 0xffff88800a662090 ":A-0001088",
-def is_same_slab_unix(cache):
-    if cache['kobj']['name'].string() == ':A-0001088':
-        slab_unix.append(cache['name'].string())
-        return True
-    return False
+class SlabInfo(gdb.Command):
 
+    def __init__(self):
+        super(SlabInfo, self).__init__("islab", gdb.COMMAND_USER)
 
-# 在 GDB 中运行此函数
-cache = find_slab_caches('UNIX')
-cpu_slab =  per_cpu_ptr(cache['cpu_slab'].cast(gdb.lookup_type('int')), 0).cast(gdb.lookup_type('struct kmem_cache_cpu').pointer()).dereference()
+    def invoke(self, slab_name, from_tty):
+        if not slab_name:
+            print("Usage: islab <slab_name>")
+            return
 
-print(cache)
+        cache = find_slab_caches(slab_name)
+        if cache is None:
+            print("未找到指定的 slab_cache")
+            return
+        gdb.execute("set $cache = *(struct kmem_cache *) {}".format(cache.address))
+        gdb.execute("set $cache_addr = (struct kmem_cache *) {}".format(cache.address))
 
-node = cpu_slab['partial'].cast(gdb.lookup_type('struct slab').pointer()).dereference()
-list = node['slab_list']
-print(node)
+        cpu_slab =  per_cpu_ptr(cache['cpu_slab'].cast(gdb.lookup_type('int')), 0).cast(gdb.lookup_type('struct kmem_cache_cpu').pointer()).dereference()
+        if cpu_slab is None:
+            print("cpu_slab 未初始化")
+            return
+        gdb.execute("set $cpu_slab = *(struct kmem_cache_cpu *) {}".format(cpu_slab.address))
+        gdb.execute("set $cpu_slab_addr = (struct kmem_cache_cpu *) {}".format(cpu_slab.address))
+
+        # node = cpu_slab['partial'].cast(gdb.lookup_type('struct slab').pointer()).dereference()
+        # if node is None:
+        #     print("slab_cache 未初始化")
+        #     return
+
+        # gdb.execute("set $node = *(struct slab *) {}".format(node.address))
+
+class PageToVirt(gdb.Command):
+    
+    def __init__(self):
+        super(PageToVirt, self).__init__("ptv", gdb.COMMAND_USER)
+    
+    def invoke(self, page, from_tty):
+        if not page:
+            print("Usage: ptv <page>")
+            return
+    
+        addr = to_virt(int(page, 16))
+        print("0x{:x}".format(addr))
+
+class VirtToPage(gdb.Command):
+
+    def __init__(self):
+        super(VirtToPage, self).__init__("vtp", gdb.COMMAND_USER)
+
+    def invoke(self, virt, from_tty):
+        if not virt:
+            print("Usage: vtp <virt>")
+            return
+
+        page = to_page(int(virt, 16))
+        print("0x{:x}".format(page))
+
+SlabInfo()
+PageToVirt()
+VirtToPage()
+SlabFreeTracePoint("slab_free").silent = True
+
